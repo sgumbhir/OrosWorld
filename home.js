c3241fd7fdc86ffda07f93130ed4614e5670e22d
sgumbhir <sgumbhir@usc.edu>
2021-04-30 16:32:16 +0530

    Home page without slider

---
+++ home.js
@@ -0,0 +1,31 @@
+const navButton = document.querySelector('.nav-button');
+const navOpen = document.querySelector('.nav-open');
+
+const tl = new TimelineLite({paused:true, reversed:true});
+
+tl.to(".cover", 1,{
+    width: "60%",
+    ease: Power2.easeOut
+}).to("nav", 1, {
+    height: "100%",
+    ease: Power2.easeOut
+}, "-=0.2")
+.fromTo('.nav-open', 0.5,{
+    opacity: 0,
+    x: 50,
+    ease: Power2.easeOut
+},{
+    opacity: 1,
+    x:  0,
+    onComplete: function(){
+        navOpen.style.pointerEvents ='auto';
+    }
+});
+
+navButton.addEventListener("click", ()=>{
+    toggleTween(tl);
+});
+
+function toggleTween(tween){
+    tween.reversed() ? tween.play() : tween.reverse();
+}
